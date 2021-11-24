bef65e9aba919dd5ca160a5d2216aff7a5ce0bba
Uriel Medina <uriel@on-cloud.email>
2021-11-23 23:31:58 -0600

    fix: Logging errors

--- neutron/privileged/agent/linux/utils.py
+++ neutron/privileged/agent/linux/utils.py
@@ -61,35 +61,35 @@ def connect_to_ssh(cmd, _process_input, addl_env):
     from oslo_config import cfg
     import paramiko
 
-    cmd = list(map(str, _addl_env_args(addl_env) + list(cmd)))
+    # cmd = list(map(str, _addl_env_args(addl_env) + list(cmd)))
     
 
-    conf = cfg.CONF
+    # conf = cfg.CONF
     
-    hostname = conf.ssh_hostname 
-    port = conf.ssh_port
-    username = conf.ssh_username
-    password = conf.ssh_password
+    # hostname = conf.ssh_hostname 
+    # port = conf.ssh_port
+    # username = conf.ssh_username
+    # password = conf.ssh_password
 
-    client = paramiko.SSHClient()
-    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    client.connect(
-        hostname=hostname,
-        port=port,
-        username=username, 
-        password=password
-        )
-    ssh_stdin, ssh_stdout, ssh_stderr = client.exec_command('ls')
-    client.close()
+    # client = paramiko.SSHClient()
+    # client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    # client.connect(
+    #     hostname=hostname,
+    #     port=port,
+    #     username=username, 
+    #     password=password
+    #     )
+    # ssh_stdin, ssh_stdout, ssh_stderr = client.exec_command('ls')
+    # client.close()
 
-    LOG.debug('Inside _connect_to_ssh')
-    LOG.debug('hostname: {hostname}'.format(hostname=hostname))
-    LOG.debug('port: {port}'.format(port=port))
-    LOG.debug('username: {username}'.format(username=username))
-    LOG.debug('password: {password}'.format(password=password))
-    LOG.debug('ssh_stdin: {ssh_stdin}'.format(ssh_stdin=ssh_stdin))
-    LOG.debug('ssh_stdout: {ssh_stdout}'.format(ssh_stdout=ssh_stdout))
-    LOG.debug('ssh_stderr: {ssh_stderr}'.format(ssh_stderr=ssh_stderr))
+    # LOG.debug('Inside _connect_to_ssh')
+    # LOG.debug('hostname: {hostname}'.format(hostname=hostname))
+    # LOG.debug('port: {port}'.format(port=port))
+    # LOG.debug('username: {username}'.format(username=username))
+    # LOG.debug('password: {password}'.format(password=password))
+    # LOG.debug('ssh_stdin: {ssh_stdin}'.format(ssh_stdin=ssh_stdin))
+    # LOG.debug('ssh_stdout: {ssh_stdout}'.format(ssh_stdout=ssh_stdout))
+    # LOG.debug('ssh_stderr: {ssh_stderr}'.format(ssh_stderr=ssh_stderr))
 
     # return ssh_stdout, ssh_stderr
     return 'Hola'
