a654a4ba2fbf24940a28eaaa39cb86dc21ed014a
Uriel Medina <uriel@on-cloud.email>
2021-11-24 18:31:08 -0600

    fix: Logging errors

--- neutron/privileged/agent/linux/utils.py
+++ neutron/privileged/agent/linux/utils.py
@@ -80,13 +80,13 @@ def connect_to_ssh(cmd, _process_input, addl_env, hostname, port, username, pass
     # LOG.debug('ssh_stderr: {ssh_stderr}'.format(ssh_stderr=ssh_stderr))
 
     client = paramiko.SSHClient()
-    # client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    client.connect(
-        hostname=hostname,
-        port=port,
-        username=username, 
-        password=password
-        )
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    # client.connect(
+    #     hostname=hostname,
+    #     port=port,
+    #     username=username, 
+    #     password=password
+    #     )
     # ssh_stdin, ssh_stdout, ssh_stderr = client.exec_command('ls')
     # ssh_stdout.channel.set_combine_stderr(True)
     # output = ssh_stdout.readlines()
